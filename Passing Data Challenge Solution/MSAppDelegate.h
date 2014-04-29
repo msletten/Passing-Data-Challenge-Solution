e9774203372e16f90f97ea79f689b65e7a029977
Mat Sletten <matsletten@MSLETTENMBP.local>
2014-04-29 15:09:25 -0700

    Initial Commit

---
+++ Passing Data Challenge Solution/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  Passing Data Challenge Solution
+//
+//  Created by Mat Sletten on 4/29/14.
+//  Copyright (c) 2014 Mat Sletten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
